8606a2a16dc0b40967ac21148eabe51268eba808
Elena Tibekina <elentibekina@gmail.com>
2020-03-26 00:13:10 +0300

    fixed problems with running

--- Pokemons/karma.conf.js
+++ Pokemons/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/Pokemon'),
+      dir: require('path').join(__dirname, './coverage/Pokemons'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
